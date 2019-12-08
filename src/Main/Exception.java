43ff4b46078d4b2ff87ca5df670bb37af7452f42
Hiroko Asato <e195712@ie.u-ryukyu.ac.jp>
2019-12-08 11:26:37 +0900

    Step2

--- src/Main/Exception.java
+++ src/Main/Exception.java
@@ -1,6 +1,12 @@
 public class Exception {
-    public static void main(String[] args) {
+public static void main(String[] args) {
+        try {
         String str = null;
         System.out.println(str.length());
-    }
+        } catch (NullPointerException e) {
+        System.out.println("NullPointerExceptionエラーが発生しました");
+        System.out.println(e.getMessage());
+        }
+}
+
 }
